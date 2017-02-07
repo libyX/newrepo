01e3e2689023c1f24b1aa0e9b2ea904e09996666
libyX <libymolxavier@gmail.com>
2017-02-07 17:28:51 -0500

    commited

--- test.java
+++ test.java
@@ -1,8 +1,8 @@
 
 public class test {
 	public static void main (String[] args){
-		for(int i=1, i<=10, i++){
-			system.out.println("Hello world"+1);
+        for(int i=1; i<=10; i++){
+			System.out.println("Hello world"+1);
 		}
 	}
 }
